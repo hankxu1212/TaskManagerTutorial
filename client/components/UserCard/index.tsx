b8180d8a91945b00f7fbce6e4608fda7a9796cff
Hank Xu <53340720+hankxu1212@users.noreply.github.com>
2026-02-06 11:30:33 -0800

    s3 links

--- client/components/UserCard/index.tsx
+++ client/components/UserCard/index.tsx
@@ -11,7 +11,7 @@ const UserCard = ({ user }: Props) => {
         <div className="flex items-center rounded border p-4 shadow">
             {user.profilePictureUrl && (
                 <Image
-                    src={`https://pm-s3-images.s3.us-east-2.amazonaws.com/p1.jpeg`}
+                    src={`https://ninghuax-tm-demo-bucket-us-west-2.s3.us-east-1.amazonaws.com/p1.jpeg`}
                     alt="profile picture"
                     width={32}
                     height={32}
--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,11 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
     images: {
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "pm-s3-images.s3.us-east-2.amazonaws.com",
+                hostname: "ninghuax-tm-demo-bucket-us-west-2.s3.us-east-1.amazonaws.com",
                 port: "",
                 pathname: "/**",
             }
