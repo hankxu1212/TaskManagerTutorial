2be354eb1c6eb247c041301553c157c7af8be9ce
Hank Xu <53340720+hankxu1212@users.noreply.github.com>
2026-02-08 01:38:54 -0800

    update ecosystem

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,12 +1,9 @@
-module.exports = {
-    apps: [
-        {
-            name: "crest",
-            script: "npm",
-            args: "run dev",
-            env: {
-                NODE_ENV: "development",
-            },
-        },
-    ],
+module.exports = { apps : [{ 
+    name: 'crest', 
+    script: 'npm',
+    args: 'run dev', 
+    env: { 
+        NODE_ENV: 'development', 
+        ENV_VAR1: 'environment-variable', 
+    } }], 
 };
